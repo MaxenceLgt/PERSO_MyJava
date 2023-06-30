d00b8d98eb7bec1641437c3e8b3718117c43f39a
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-06-30 15:33:11 +0200

    feat(main): Add test file that help me to test my features

---
+++ Piscine_Java/Day02/Test.java
@@ -0,0 +1,9 @@
+import ex_05.Gecko;
+
+public class Test {
+    public static void main (String[] av) {
+            Gecko mimi = new Gecko("mimi");
+            mimi.hello("Titi");
+            mimi.hello(2);
+    }
+}
