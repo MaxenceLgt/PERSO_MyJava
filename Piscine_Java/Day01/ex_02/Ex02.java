4a2fbafffce7a08c7ab4dff9ec506adbdc079fbc
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-06-30 10:37:52 +0200

    refactor(main): Modify variable name

--- Piscine_Java/Day01/ex_02/Ex02.java
+++ Piscine_Java/Day01/ex_02/Ex02.java
@@ -2,10 +2,10 @@ package ex_02;
 
 public class Ex02 {
     public static String getAngryDog(int nb) {
-        String woof_str = "";
+        String woofStr = "";
 
         for (int i = 0; i < nb; i++)
-            woof_str += "woof";
-        return (woof_str);
+            woofStr += "woof";
+        return (woofStr);
     }
 }
