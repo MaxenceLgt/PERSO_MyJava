8b886b76b031e1e67eb0969d1995947ee9772598
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-07-03 15:02:35 +0200

    feat(main): Add exercise 6 to initiate a Team class to add or remove astronaut from the team or check if an astronaut is working or is in standby

---
+++ Piscine_Java/Day03/Team.java
@@ -0,0 +1,52 @@
+package Day03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    private String teamName = null;
+    private List<Day03.Astronaut> astroList = new ArrayList<Day03.Astronaut>();
+    public Team(String nameOfTeam) {
+        teamName = nameOfTeam;
+    }
+
+    public String getName() {
+        return (teamName);
+    }
+
+    public void add(Day03.Astronaut newAstro) {
+        if (newAstro != null) {
+            astroList.add(newAstro);
+        }
+    }
+
+    public void remove(Day03.Astronaut astronaut) {
+        if (astronaut == null)
+            return;
+        for (int i = 0; i < astroList.size(); i++) {
+            if (astroList.get(i).equals(astronaut)) {
+                astroList.remove(i);
+            }
+        }
+    }
+
+    public int countMembers() {
+        return (astroList.size());
+    }
+
+    public void showMembers() {
+        if (astroList.size() == 0)
+            return;
+        System.out.print(teamName + ":");
+        for (int i = 0; i < astroList.size(); i++) {
+            if (astroList.get(i).getDestination() != null)
+                System.out.print(" " + astroList.get(i).getName() + " on mission");
+            else
+                System.out.print(" " + astroList.get(i).getName() + " on standby");
+            if (i == (astroList.size() - 1))
+                System.out.println(".");
+            else
+                System.out.print(",");
+        }
+    }
+}
