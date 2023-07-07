0bb2ab96b6d3b5988d2117408b2ebebdb3c9cb16
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-07-07 14:48:13 +0200

    refactor(main): Finish SpaceMarine class with override methods

--- Piscine_Java/Day05/SpaceMarine.java
+++ Piscine_Java/Day05/SpaceMarine.java
@@ -23,6 +23,33 @@ abstract class SpaceMarine extends Unit {
             System.out.println(getName() + ": Hey, this is crazy. I'm not going to fight this empty-handed.");
             return (false);
         }
-        return (true);
+        if (weapon.isMelee() == true && this.targetCloseTo != fighter) {
+            System.out.println(getName() + ": I ' m too far away from " + fighter.getName() + ".");
+            return (false);
+        }
+        if (weapon.getApcost() <= getAp()) {
+            System.out.println(getName() + " attacks " + fighter.getName() + " with a " + weapon.getName() + ".");
+            this.ap -= weapon.getApcost();
+            weapon.attack();
+            fighter.receiveDamage(weapon.getDamage());
+            return (true);
+        }
+        return (false);
+    }
+
+    @Override public Boolean moveCloseTo(Fighter fighter) {
+        if (weapon.isMelee()) {
+            return (super.moveCloseTo(fighter));
+        }
+        return (false);
+    }
+
+    @Override public void recoverAP() {
+        if (this.ap < 48) {
+            this.ap += 2;
+        } else {
+            this.ap = 50;
+        }
+        super.recoverAP();
     }
 }
